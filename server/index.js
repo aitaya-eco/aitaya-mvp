10c595486ee0737580f3beb52dee5d2ad8178dc2
richardcreatech <richardcreatech@gmail.com>
2026-08-04 14:52:17 +0100

    fix: made a few changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const app = express();
-const {auth_router} = require("./routes/auth");
-const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+const { createClient } = require("@supabase/supabase-js");
+const dotenv = require("dotenv");
+const { auth_router } = require("./routes/auth/auth");
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send("Hello, World!");
 });
 app.use("/auth", auth_router);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const { registerUser } = require("../../controller/auth/authController");
 const auth_router = express.Router();
-const { registerUser } = require("../controllers/authController");
 
 // This is the route for user registration
 auth_router.post("/register", registerUser);
 
-module.exports = { auth_router };
+module.exports = { auth_router  };
