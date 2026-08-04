f95e4ee378f81877a71a17fffbb82a6a938fcbae
richardcreatech <richardcreatech@gmail.com>
2026-08-04 15:08:43 +0100

    feat: tested the signup controller

--- server/controller/auth/authController.js
+++ server/controller/auth/authController.js
@@ -1,6 +1,20 @@
+const {supabase} = require("../../config/supabase");
+
 const registerUser = (req, res) => {
   // Logic for registering a user goes here
-  res.send("User registered successfully!");
+  const {email, password, confirmPassword} = req.body;
+
+  // This checks for empty fields and returns an error message if any of the required fields are missing.
+  if (!email || !password || !confirmPassword) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // This compares the password and confirmPassword fields to ensure they match. If they don't match, it returns an error message indicating that the passwords do not match.
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+
+  res.json({ message: `${email.split("@")[0]} registered successfully` });
 };
 
 module.exports = { registerUser };
