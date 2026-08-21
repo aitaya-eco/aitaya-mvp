29708c3e5fac9e2b3f0705e54fd12b1be1384e2c
aitaya-eco <aitaya.main@gmail.com>
2026-08-21 12:49:54 +0100

    Update authController.js: Added the logic for logging in the user
    Added the login logic

--- server/controller/auth/authController.js
+++ server/controller/auth/authController.js
@@ -227,6 +227,38 @@ const createPassword = async (req, res) => {
 
 const loginUser = (req, res) => {
   // This is the logic for Logging in a user goes here
+
+  try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({error: "Email and Password are Required"});
+        }
+
+        const normalisedEmail = email.trim().toLowerCase();
+
+        const {data, error} = await supabase.auth.signInWithPassword({
+            email: normalisedEmail,
+            password});
+
+        if (error) {
+            return res.status(401).json({message: error.message || "Invalid Email or Password",});
+        }
+
+        res.status(200).json({
+            message: "Login Successful",
+            user: data.user,
+            access_token: data.session.access_token,
+            refresh_token: data.session.refresh_token,
+        });
+    }
+
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Server error while logging in",
+        });
+    }
 };
 
 module.exports = { registerUserEmail, confirmOTP, createPassword, loginUser };
